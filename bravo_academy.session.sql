74beb9312116ca12ae8dc626a8cce02e15b4c74a
ctwtc <teddytung518@gmail.com>
2023-01-23 02:38:31 +0800

    Merge branch 'main' of github.com:Hermanyim1217/groupProject

---
+++ bravo_academy.session.sql
@@ -0,0 +1,92 @@
+CREATE TABLE users
+(
+  user_id     SERIAL  NOT NULL,
+  email       VARCHAR NOT NULL,
+  password    VARCHAR NOT NULL,
+  username    VARCHAR NOT NULL,
+  phone       INTEGER NOT NULL,
+  description VARCHAR     ,
+  price       INTEGER NOT NULL DEFAULT 100,
+  duration    INTEGER NOT NULL DEFAULT 60,
+  image       VARCHAR      ,
+  role_id     INTEGER      ,
+  subject_id  INTEGER  ,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE subject
+(
+  id           SERIAL  NOT NULL,
+  subject_name VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role
+(
+  id        SERIAL  NOT NULL,
+  role_name VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
+        
+CREATE TABLE bookmark_table
+(
+  student_id INTEGER,
+  teacher_id INTEGER 
+);
+
+CREATE TABLE comments
+(
+  comment_id SERIAL NOT NULL,
+  user_id    INTEGER,
+  PRIMARY KEY (comment_id)
+);
+
+CREATE TABLE user_comment_table
+(
+  user_id    INTEGER,
+  comment_id INTEGER 
+);
+
+ALTER TABLE user_comment_table
+  ADD CONSTRAINT FK_users_TO_user_comment_table
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id);
+
+ALTER TABLE user_comment_table
+  ADD CONSTRAINT FK_comments_TO_user_comment_table
+    FOREIGN KEY (comment_id)
+    REFERENCES comments (comment_id);
+
+ALTER TABLE bookmark_table
+  ADD CONSTRAINT FK_users_TO_bookmark_table
+    FOREIGN KEY (student_id)
+    REFERENCES users (user_id);
+
+ALTER TABLE bookmark_table
+  ADD CONSTRAINT FK_users_TO_bookmark_table1
+    FOREIGN KEY (teacher_id)
+    REFERENCES users (user_id);
+
+
+
+ALTER TABLE users
+  ADD CONSTRAINT FK_role_TO_users
+    FOREIGN KEY (role_id)
+    REFERENCES role (id);
+
+ALTER TABLE users
+  ADD CONSTRAINT FK_subject_TO_users
+    FOREIGN KEY (subject_id)
+    REFERENCES subject (id);
+
+
+
+INSERT INTO subject (subject_name) VALUES ('chinese');
+INSERT INTO subject (subject_name) VALUES ('english');
+INSERT INTO subject (subject_name) VALUES ('french');
+INSERT INTO subject (subject_name) VALUES ('japanese');
+INSERT INTO subject (subject_name) VALUES ('student');
+
+INSERT INTO role (role_name) VALUES ('teacher');
+INSERT INTO role (role_name) VALUES ('student');
