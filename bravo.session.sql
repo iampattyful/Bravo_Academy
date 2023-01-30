eaa559ee015f237ca894672ab81aa70f453e70c7
Herman Yim <yim0_0herman@yahoo.com.hk>
2023-01-30 11:31:26 +0800

    create tables

--- bravo.session.sql
+++ bravo.session.sql
@@ -35,12 +35,12 @@ CREATE TABLE bookmark_table
   teacher_id INTEGER 
 );
 
-CREATE TABLE comments
-(
-  comment_id SERIAL NOT NULL,
-  user_id    INTEGER,
-  PRIMARY KEY (comment_id)
-);
+-- CREATE TABLE comments
+-- (
+--   comment_id SERIAL NOT NULL,
+--   user_id    INTEGER,
+--   PRIMARY KEY (comment_id)
+-- );
 
 CREATE TABLE user_comment_table
 (
@@ -91,3 +91,25 @@ INSERT INTO subject (subject_name) VALUES ('student');
 INSERT INTO role (role_name) VALUES ('teacher');
 INSERT INTO role (role_name) VALUES ('student');
 
+DROP TABLE user_comment_table;
+DROP TABLE comments;
+
+CREATE TABLE user_comment_table
+(
+  user_id    INTEGER,
+  content    VARCHAR NOT NULL,
+  comment_id SERIAL  NOT NULL,
+  PRIMARY KEY (comment_id)
+);
+ALTER TABLE user_comment_table
+  ADD CONSTRAINT FK_users_TO_user_comment_table
+    FOREIGN KEY (user_id)
+    REFERENCES users (user_id);
+
+CREATE TABLE contact_us
+(
+  id      SERIAL  NOT NULL,
+  email   VARCHAR NOT NULL,
+  content VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
