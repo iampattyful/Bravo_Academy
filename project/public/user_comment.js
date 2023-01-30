1ee558cecce3d937998160c6c14b781d12ba1baa
Patty Lau <iampattyful@gmail.com>
2023-01-30 19:20:20 +0800

    added user_comment.js

--- project/public/user_comment.js
+++ project/public/user_comment.js
@@ -1,6 +1,8 @@
+let checkLoginRes;
 // window onload
 window.onload = async function () {
-  const res = await fetch(`/user_comment/}`, {
+  checkLoginRes = await checkLogin();
+  const res = await fetch(`/user_comment`, {
     method: "GET",
   });
   let json = await res.json();
@@ -8,13 +10,17 @@ window.onload = async function () {
 };
 
 //user comment template
-const userCommentTemplate = (
-  img,
-  userId,
-  userName,
-  userComment
-) =>
-  `           
+const userCommentTemplate = (img, userName, userComment) =>
+  ` 
+  <div class="commentBox">
+    <div class="commentUserInfo">
+        <div class="commentPic"><img src="${img}"></div>
+        <div class="commentContent">
+            <div class="commentName">${userName}</div>
+        </div>
+    </div>
+    <div class="commentText">${userComment}</div>
+</div>          
       `;
 
 //user comment result
@@ -28,54 +34,21 @@ async function userCommentResult(json) {
       .map(
         (comment) =>
           `${
-            comments.image === null
-              ? teacherProfileSettingsTemplate(
+            comment.image === null
+              ? userCommentTemplate(
                   `./assets/profile_image_placeholder.jpg" alt=""`,
-                  teacher.subject_id,
-                  teacher.subject_name,
-                  teacher.user_id,
-                  teacher.username,
-                  teacher.description,
-                  teacher.price,
-                  teacher.duration
+                  comment.username,
+                  comment.content
                 )
-              : teacherProfileSettingsTemplate(
-                  teacher.image,
-                  teacher.subject_id,
-                  teacher.subject_name,
-                  teacher.user_id,
-                  teacher.username,
-                  teacher.description,
-                  teacher.price,
-                  teacher.duration
+              : userCommentTemplate(
+                  comment.image,
+                  comment.username,
+                  comment.content
                 )
           }`
       )
       .join("");
-
-    const profileDiv = document.querySelector("div#teacherContainer");
-    //for (const profileDiv of profileDivs) {
-    //const checkLoginRes = await checkLogin();
-    if (checkLoginRes.result && checkLoginRes.users.role_id == 2) {
-      //console.log(await checkLogin());
-      console.log(profileDiv.querySelector("div.iconClicked"));
-      profileDiv
-        .querySelector("div.iconClicked")
-        .addEventListener("click", async (event) => {
-          let userId = event.currentTarget.dataset.id;
-          console.log(userId);
-          profileDiv.querySelector(
-            ".iconClicked"
-          ).innerHTML = `<i class="fa-solid fa-bookmark"></i>`;
-          const res = await fetch(`/bookmark/${userId}`, {
-            method: "POST",
-          });
-        });
-    } else {
-      profileDiv.querySelector(".fa-bookmark").classList.add("hide");
-    }
-    //}
   } else {
-    teacherProfileContent.innerHTML = "";
+    userCommentContent.innerHTML = "";
   }
 }
