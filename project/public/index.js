29779c33f2aa9baee4683c0ed12af67bb0933f81
Herman Yim <yim0_0herman@yahoo.com.hk>
2023-01-31 19:06:30 +0800

    updated usercomment navbar

--- project/public/index.js
+++ project/public/index.js
@@ -33,7 +33,6 @@ const checkLogin = async () => {
       document.querySelector("#logout").classList.remove("hide");
       document.querySelector("#loginShow").classList.add("hide");
       document.querySelector("#signUpShow").classList.add("hide");
-      document.querySelector("#review").classList.remove("hide");
       // document.querySelector(".become-teacher").classList.add("hide");
       return json;
     }
