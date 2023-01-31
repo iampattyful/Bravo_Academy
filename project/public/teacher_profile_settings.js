ec83404009e93e218e62bd4355243015bdd02cef
Patty Lau <iampattyful@gmail.com>
2023-01-31 22:07:06 +0800

    patty fixed submit blank feedback bug

--- project/public/teacher_profile_settings.js
+++ project/public/teacher_profile_settings.js
@@ -73,9 +73,10 @@ submitUserComment.addEventListener("submit", async (event) => {
 
   const content = await submitUserCommentRes.json();
   if (content.result) {
-    submitUserComment.reset();
+    console.log(content.result)
+    submitUserComment.reset();    
+    alert("已成功提交用戶評價！");
     // window.location = `/teacher_profile_settings.html?id=${checkLoginRes.users.id}`;
-    return;
   } else {
     alert("Please fill in the blank.");
   }
